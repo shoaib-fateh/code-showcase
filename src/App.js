0cce2f5965079ada6911fcf6a109ab1c156f92ac
Shoaib Fateh <shoaibfateh21@yahoo.com>
2024-08-03 20:29:27 +0430

    added tailwind css / HomePage Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import HomePage from "./components/homepage/home-page";
 
 const App = () => {
-	return <div>Hello World</div>;
+	return (
+		<div className="container mx-auto">
+			<HomePage />
+		</div>
+	);
 };
 
 export default App;
