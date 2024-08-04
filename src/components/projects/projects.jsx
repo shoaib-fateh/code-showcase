1edc0f34fb9e25423b3e85b0ed77f4c81bd1af1c
Shoaib Fateh <shoaibfateh21@yahoo.com>
2024-08-04 08:00:24 +0430

    added projects section / project component

---
+++ src/components/projects/projects.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Project from "../project/project";
+
+const Projects = () => (
+	<div className="grid grid-cols-1 md:grid-cols-3 gap-4 px-5">
+		<Project
+			src="https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png"
+			to="https://github.com/shoaib-fateh/herai_rafah_ict"
+			title="Herai Rafah ICT"
+			description="Herai Rafah ICT is a comprehensive project aimed at integrating information and communication technology (ICT) solutions for Herai Rafah. This repository will serve as a centralized location for all project files, documentation, and development history."
+		/>
+		<Project
+			src="https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png"
+			to="https://github.com/shoaib-fateh/line-civic-institution"
+			title="Line Civic Institution."
+			description="Civic empowerment at Line Civic Institution. A non-profit initiative promoting democracy, social justice, and community building. Join us for positive change"
+		/>
+		<Project
+			src="https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png"
+			to="https://github.com/shoaib-fateh/dark-calculator"
+			title="Dark Calculator"
+			description="This is an advanced JavaScript calculator with a user-friendly interface, designed to perform basic arithmetic operations. The calculator incorporates features such as history tracking and a respon"
+		/>
+		<Project
+			src="https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png"
+			to="https://github.com/shoaib-fateh/xylophone"
+			title="Xylophone"
+			description="A web app featuring a virtual xylophone, allowing users to play musical notes with interactive keys. Implemented in HTML, CSS, and JavaScript. Publicly accessible for creative exploration and"
+		/>
+	</div>
+);
+
+export default Projects;
--- src/pages/homepage/home-page.jsx
+++ src/pages/homepage/home-page.jsx
@@ -9,48 +9,55 @@ import {
 } from "@fortawesome/free-brands-svg-icons";
 
 import Section from "../../components/section/section";
+import Projects from "../../components/projects/projects";
 
 const HomePage = () => (
-	<section>
-		<div className="flex md:flex-row items-center justify-center p-4">
-			<div className="flex-2 px-[30px]">
-				<Section
-					title="Full-stack web and mobile app developer, and amateur
+	<>
+		<section>
+			<div className="flex md:flex-row items-center justify-center p-4">
+				<div className="flex-2 px-[30px]">
+					<Section
+						title="Full-stack web and mobile app developer, and amateur
 					astronaut."
-					description="As a React developer, I have a strong foundation in building
+						description="As a React developer, I have a strong foundation in building
 					dynamic and responsive web applications. I enjoy utilizing
 					my skills in JavaScript, HTML5, CSS3, and various frameworks
 					to create user-friendly interfaces. I am also experienced in
 					backend development with Node.js, ensuring that my
 					applications are both scalable and secure."
-				/>
-				<div className="flex md:flex-row p-4">
-					<SocialIcon
-						url="https://github.com/shoaib-fateh"
-						icon={faGithub}
 					/>
+					<div className="flex md:flex-row p-4">
+						<SocialIcon
+							url="https://github.com/shoaib-fateh"
+							icon={faGithub}
+						/>
 
-					<SocialIcon
-						url="instagram.com/shoaibfateh21"
-						icon={faInstagram}
-					/>
+						<SocialIcon
+							url="instagram.com/shoaibfateh21"
+							icon={faInstagram}
+						/>
 
-					<SocialIcon
-						url="https://t.me/shoaibfateh21"
-						icon={faTelegramPlane}
-					/>
+						<SocialIcon
+							url="https://t.me/shoaibfateh21"
+							icon={faTelegramPlane}
+						/>
 
-					<SocialIcon
-						url="https://www.linkedin.com/in/shoaib-fateh/"
-						icon={faLinkedinIn}
+						<SocialIcon
+							url="https://www.linkedin.com/in/shoaib-fateh/"
+							icon={faLinkedinIn}
+						/>
+					</div>
+				</div>
+				<div className="flex-2 py-[30px]">
+					<img
+						src="./homepage.jpg"
+						className="rotate-3 rounded-[50px]"
 					/>
 				</div>
 			</div>
-			<div className="flex-2 py-[30px]">
-				<img src="./homepage.jpg" className="rotate-3 rounded-[50px]" />
-			</div>
-		</div>
-	</section>
+		</section>
+		<Projects />
+	</>
 );
 
 export default HomePage;
