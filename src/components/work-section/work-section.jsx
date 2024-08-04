419822ddc154463448a4974d4a2923dfeaed64d9
Shoaib Fateh <shoaibfateh21@yahoo.com>
2024-08-04 09:11:00 +0430

    added work section component / HomePage page completed!

--- src/components/work-section/work-section.jsx
+++ src/components/work-section/work-section.jsx
@@ -19,13 +19,34 @@ const WorkSection = () => (
 			/>
 		</div>
 		<div>
-			<FontAwesomeIcon icon={faBagShopping} />
-			<br />
-			facebook Facebook Software Engineer 2019 - Present
-			<br />
-			twitter Twitter Software Engineer 2019 - Present
+			<div>
+				<FontAwesomeIcon
+					icon={faBagShopping}
+					className="text-gray-900 text-xl"
+				/>
+				<span className="px-2 text-gray-900 font-bold">Work</span>
+			</div>
+
+			<Work
+				src="./facebook.png"
+				title="Facebook"
+				job="Software Engineer"
+			/>
+
+			<Work src="./twitter.png" title="Twitter" job="Software Engineer" />
 		</div>
 	</div>
 );
 
 export default WorkSection;
+
+const Work = ({ src, title, job }) => (
+	<div className="py-5 flex">
+		<img src={src} alt="facebook" className="w-12 rounded-full mr-5" />
+
+		<div className="flex-grow text-gray-700">
+			<h1 className="font-bold">{title}</h1>
+			<span>{job}</span>
+		</div>
+	</div>
+);
